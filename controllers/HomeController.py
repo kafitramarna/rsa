b83380f15665c9d8f08d390dfee6067bafa8c910
kafitramarna <kafitra.marna@gmail.com>
2024-05-31 19:40:25 +0700

    ubah affine chiper ke rsa

--- controllers/HomeController.py
+++ controllers/HomeController.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QDialog, QMessageBox
 from PyQt5.uic import loadUi
-import string
+import rsa
+import base64
 
 class HomeController(QDialog):
     def __init__(self, parent=None):
@@ -9,87 +10,53 @@ class HomeController(QDialog):
         self.btnEncrypt.clicked.connect(self.encrypt)
         self.btnDecrypt.clicked.connect(self.decrypt)
         self.btnClear.clicked.connect(self.clear)
-        self.btnKunciEncrypt.clicked.connect(self.encrypt_key)
-        self.btnKunciDecrypt.clicked.connect(self.decrypt_key)
+        self.btnGenerateKeys.clicked.connect(self.generate_keys)
     
     def encrypt(self):
         try:
-            plain = self.txtPlain.text().replace(' ', '')
-            if not plain.isalpha():
-                raise ValueError("Input harus berupa huruf dan key tidak boleh memiliki spasi")
-            m = int(self.txtM.text())
-            if (not(self.is_prime(m))):
-                raise ValueError("nilai m harus relatif prima terhadap 26")
-            b = int(self.txtB.text())
-            
-            alfabet = string.ascii_lowercase
-            chiper = ''
-            for i in range(len(plain)):
-                chiper += alfabet[((alfabet.index(plain[i].lower()) * m) + b) % 26]
+            plain = self.txtPlain.text().encode('utf-8')
+            public_key = self.get_public_key()
+            chiper = rsa.encrypt(plain, public_key)
+            chiper = base64.b64encode(chiper).decode('utf-8')
             self.txtChiper.setText(chiper)
-        except ValueError as e:
+        except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
         
     def decrypt(self):
         try:
-            chiper = self.txtChiper.text().replace(' ', '')
-            if not chiper.isalpha():
-                raise ValueError("Input harus berupa huruf dan key tidak boleh memiliki spasi")
-            
-            m = int(self.txtM.text())
-            if not self.is_prime(m):
-                raise ValueError("Nilai m harus relatif prima terhadap 26")
-            b = int(self.txtB.text())
-            m_inv = 0
-            for i in range(26):
-                if (m * i) % 26 == 1:
-                    m_inv = i
-                    break
-            alfabet = string.ascii_lowercase
-            plain = ''
-            for i in range(len(chiper)):
-                plain += alfabet[m_inv * (alfabet.index(chiper[i].lower()) - b) % 26]
+            chiper_b64 = self.txtChiper.text()
+            private_key = self.get_private_key()
+            chiper = base64.b64decode(chiper_b64)
+            plain = rsa.decrypt(chiper, private_key).decode('utf-8')
             self.txtPlain2.setText(plain)
-        except ValueError as e:
+        except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
     
     def clear(self):
         self.txtPlain.clear()
-        self.txtM.clear()
-        self.txtB.clear()
         self.txtChiper.clear()
         self.txtPlain2.clear()
-    
-    def is_prime(self, num, i=2):
-        if num <= 2:
-            return num == 2
-        if num % i == 0:
-            return False
-        if i * i > num:
-            return True
-        return self.is_prime(num, i + 1)
+        self.txtPublicKey.clear()
+        self.txtPrivateKey.clear()
 
-    def encrypt_key(self):
+    def generate_keys(self):
         try:
-            m = int(self.txtM.text())
-            if (not(self.is_prime(m))):
-                raise ValueError("nilai m harus relatif prima terhadap 26")
-            b = int(self.txtB.text())
-            QMessageBox.information(self, "Key", f"m: {m}\nb: {b}")
-        except ValueError as e:
+            (public_key, private_key) = rsa.newkeys(512)
+            self.txtPublicKey.setText(public_key.save_pkcs1().decode('utf-8'))
+            self.txtPrivateKey.setText(private_key.save_pkcs1().decode('utf-8'))
+        except Exception as e:
             QMessageBox.critical(self, "Error", str(e))
-    
-    def decrypt_key(self):
+
+    def get_public_key(self):
         try:
-            m = int(self.txtM.text())
-            if not self.is_prime(m):
-                raise ValueError("Nilai m harus relatif prima terhadap 26")
-            b = int(self.txtB.text())
-            m_inv = 0
-            for i in range(26):
-                if (m * i) % 26 == 1:
-                    m_inv = i
-                    break
-            QMessageBox.information(self, "Key", f"m: {m_inv}\nb: {b}")
-        except ValueError as e:
-            QMessageBox.critical(self, "Error", str(e))
+            public_key_data = self.txtPublicKey.text().encode('utf-8').replace(b'\\n', b'\n')
+            return rsa.PublicKey.load_pkcs1(public_key_data)
+        except Exception as e:
+            raise ValueError("Invalid public key")
+
+    def get_private_key(self):
+        try:
+            private_key_data = self.txtPrivateKey.text().encode('utf-8').replace(b'\\n', b'\n')
+            return rsa.PrivateKey.load_pkcs1(private_key_data)
+        except Exception as e:
+            raise ValueError("Invalid private key")
